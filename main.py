4fe752aba532d3295a599896897460936a43ac0a
Naman Saini <saininaman98@gmail.com>
2019-11-14 13:50:04 +0530

    Changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import tensorflow as tf
 data=pd.read_csv("fake.csv", usecols=[2,4,5,8,19])
 test_data=data.sample(frac=0.3,random_state=200)
 train_data=data.drop(test_data.index)
@@ -93,31 +93,199 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras import regularizers
-sequence_length_title = X_train_title.shape[1]
-sequence_length_text=X_train_text.shape[1]
-filter_sizes = [3,4,5]
-num_filters = 100
-drop = 0.5
 
 
-#title layer
-inputs_title = Input(shape=(sequence_length_title,))
-embedding_title = embedding_layer(inputs_title)
-reshape_title = Reshape((sequence_length_title,EMBEDDING_DIM,1))(embedding)
 
-conv_0_title = Conv2D(num_filters, (filter_sizes[0], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
-conv_1_title = Conv2D(num_filters, (filter_sizes[1], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
-conv_2_title = Conv2D(num_filters, (filter_sizes[2], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
 
-maxpool_0_title = MaxPooling2D((sequence_length_title - filter_sizes[0] + 1, 1), strides=(1,1))(conv_0_title)
-maxpool_1_title = MaxPooling2D((sequence_length_title - filter_sizes[1] + 1, 1), strides=(1,1))(conv_1_title)
-maxpool_2_title = MaxPooling2D((sequence_length_title - filter_sizes[2] + 1, 1), strides=(1,1))(conv_2_title)
+class TempEnsemModel(tf.keras.Model):
+    """ Class for defining eager compatible tfrecords file
+        I did not use tfe.Network since it will be depracated in the
+        future by tensorflow.
+    """
 
-merged_tensor_title = concatenate([maxpool_0_title, maxpool_1_title, maxpool_2_title], axis=1)
-flatten = Flatten()(merged_tensor)
-reshape = Reshape((3*num_filters,))(flatten)
-dropout = Dropout(drop)(flatten)
-output = Dense(units=3, activation='softmax',kernel_regularizer=regularizers.l2(0.01))(dropout)
+    def __init__(self):
+        """ Init
+            Set all the layers that need to be tracked in the process of
+            gradients descent (pooling and dropout for example dont need
+            to be stored)
+        """
+
+        super(TempEnsemModel, self).__init__()
+        
+
+    def __aditive_gaussian_noise(self, input, std):
+        """ Function to add additive zero mean noise as described in the paper
+        Arguments:
+            input {tensor} -- image
+            std {int} -- std to use in the random_normal
+        Returns:
+            {tensor} -- image with added noise
+        """
+
+        noise = tf.random_normal(shape=tf.shape(
+            input), mean=0.0, stddev=std, dtype=tf.float32)
+        return input + noise
+
+
+    def call(self, X_train_title,X_train_text, training=True):
+        """ Function that allows running a tensor through the pi model
+        Arguments:
+            input {[tensor]} -- batch of images
+            training {bool} -- if true applies augmentaton and additive noise
+        Returns:
+            [tensor] -- predictions
+        """
+
+        
+            
+        
+        sequence_length_title = X_train_title.shape[1]
+        sequence_length_text=X_train_text.shape[1]
+        filter_sizes = [2,3,4]
+        num_filters = 100
+        drop = 0.2
+        
+        
+        #title layer
+        inputs_title = Input(shape=(sequence_length_title,))
+        embedding_title = embedding_layer(inputs_title)
+        reshape_title = Reshape((sequence_length_title,EMBEDDING_DIM,1))(embedding_title)
+        if training:
+            reshape_title = self.__aditive_gaussian_noise(reshape_title, 0.15)
+        
+        conv_0_title = Conv2D(num_filters, (filter_sizes[0], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
+        conv_1_title = Conv2D(num_filters, (filter_sizes[1], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
+        conv_2_title = Conv2D(num_filters, (filter_sizes[2], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_title)
+        
+        maxpool_0_title = MaxPooling2D((sequence_length_title - filter_sizes[0] + 1, 1), strides=(1,1))(conv_0_title)
+        maxpool_1_title = MaxPooling2D((sequence_length_title - filter_sizes[1] + 1, 1), strides=(1,1))(conv_1_title)
+        maxpool_2_title = MaxPooling2D((sequence_length_title - filter_sizes[2] + 1, 1), strides=(1,1))(conv_2_title)
+        
+        merged_tensor_title = concatenate([maxpool_0_title, maxpool_1_title, maxpool_2_title], axis=1)
+        flatten = Flatten()(merged_tensor_title)
+        reshape = Reshape((3*num_filters,))(flatten)
+        dense_title = Dense(50, activation='relu', kernel_regularizer='l2', name='DenseTitle')(reshape)
+        
+        #text layer
+        inputs_text = Input(shape=(sequence_length_text,))
+        embedding_text = embedding_layer(inputs_text)
+        reshape_text = Reshape((sequence_length_text,EMBEDDING_DIM,1))(embedding_text)
+        
+        if training:
+            reshape_text = self.__aditive_gaussian_noise(reshape_text, 0.15)
+            
+        conv_0_text = Conv2D(num_filters, (filter_sizes[0], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_text)
+        conv_1_text = Conv2D(num_filters, (filter_sizes[1], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_text)
+        conv_2_text = Conv2D(num_filters, (filter_sizes[2], EMBEDDING_DIM),activation='relu',kernel_regularizer=regularizers.l2(0.01))(reshape_text)
+        
+        maxpool_0_text = MaxPooling2D((sequence_length_text - filter_sizes[0] + 1, 1), strides=(1,1))(conv_0_text)
+        maxpool_1_text = MaxPooling2D((sequence_length_text - filter_sizes[1] + 1, 1), strides=(1,1))(conv_1_text)
+        maxpool_2_text = MaxPooling2D((sequence_length_text - filter_sizes[2] + 1, 1), strides=(1,1))(conv_2_text)
+        
+        merged_tensor_text = concatenate([maxpool_0_text, maxpool_1_text, maxpool_2_text], axis=1)
+        flatten = Flatten()(merged_tensor_text)
+        reshape = Reshape((3*num_filters,))(flatten)
+        dense_text = Dense(100, activation='relu', kernel_regularizer='l2', name='DenseText')(reshape)
+        
+        x = concatenate([dense_title, dense_text])
+        
+        #Common part
+        x = Dense(50, activation='relu')(x)
+        x = Dropout(drop)(x)
+        x = Dense(50, activation='relu')(x)
+        x = Dropout(drop)(x)
+        out = Dense(4, activation='softmax')(x)
+        return out
+
+
+
+
+
+
+
+
+def temporal_ensembling_loss(X_train_labeled, y_train_labeled, X_train_unlabeled, model, unsupervised_weight, ensembling_targets):
+    """ Gets the loss for the temporal ensembling model
+    Arguments:
+        X_train_labeled {tensor} -- labeled samples
+        y_train_labeled {tensor} -- labeled train labels
+        X_train_unlabeled {tensor} -- unlabeled samples 
+        model {tf.keras.Model} -- temporal ensembling model
+        unsupervised_weight {float} -- weight of the unsupervised loss
+        ensembling_targets {np.array} --  ensembling targets
+    Returns:
+        {tensor} -- predictions for the ensembles
+        {tensor} -- loss value
+    """
+
+    z_labeled = model(X_train_labeled)
+    z_unlabeled = model(X_train_unlabeled)
+
+    current_predictions = tf.concat([z_labeled, z_unlabeled], 0)
+
+    return current_predictions, tf.losses.softmax_cross_entropy(
+        y_train_labeled, z_labeled) + unsupervised_weight * (
+            tf.losses.mean_squared_error(ensembling_targets, current_predictions))
+
+
+def temporal_ensembling_gradients(X_train_labeled, y_train_labeled, X_train_unlabeled, model, unsupervised_weight, ensembling_targets):
+    """ Gets the gradients for the temporal ensembling model
+    Arguments:
+        X_train_labeled {tensor} -- labeled samples
+        y_train_labeled {tensor} -- labeled train labels
+        X_train_unlabeled {tensor} -- unlabeled samples 
+        model {tf.keras.Model} -- temporal ensembling model
+        unsupervised_weight {float} -- weight of the unsupervised loss
+        ensembling_targets {np.array} --  ensembling targets
+    Returns:
+        {tensor} -- predictions for the ensembles
+        {tensor} -- loss value
+        {tensor} -- gradients for each model variables
+    """
+
+    with tf.GradientTape() as tape:
+        ensemble_precitions, loss_value = temporal_ensembling_loss(X_train_labeled, y_train_labeled, X_train_unlabeled,
+                                                                   model, unsupervised_weight, ensembling_targets)
+
+    return ensemble_precitions, loss_value, tape.gradient(loss_value, model.variables)
+
+
+
+def ramp_up_function(epoch, epoch_with_max_rampup=80):
+    """ Ramps the value of the weight and learning rate according to the epoch
+        according to the paper
+    Arguments:
+        {int} epoch
+        {int} epoch where the rampup function gets its maximum value
+    Returns:
+        {float} -- rampup value
+    """
+
+    if epoch < epoch_with_max_rampup:
+        p = max(0.0, float(epoch)) / float(epoch_with_max_rampup)
+        p = 1.0 - p
+        return math.exp(-p*p*5.0)
+    else:
+        return 1.0
+
+
+def ramp_down_function(epoch, num_epochs):
+    """ Ramps down the value of the learning rate and adam's beta
+        in the last 50 epochs according to the paper
+    Arguments:
+        {int} current epoch
+        {int} total epochs to train
+    Returns:
+        {float} -- rampup value
+    """
+    epoch_with_max_rampdown = 50
+
+    if epoch >= (num_epochs - epoch_with_max_rampdown):
+        ep = (epoch - (num_epochs - epoch_with_max_rampdown)) * 0.5
+        return math.exp(-(ep * ep) / epoch_with_max_rampdown)
+    else:
+        return 1.0
+
 
 # this creates a model that includes
 model = Model(inputs, output)
